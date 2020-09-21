18e3298b61f57f92dec4b34fb71dbd84607db0ff
Novalinium <nova@noblejury.com>
2020-09-21 14:35:54 -0400

    Better way

--- ca-covid.sh
+++ ca-covid.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-curl -s https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv | fgrep -e Alameda -e Contra\ Costa -e Marin -e Napa -e San\ Francisco -e San\ Mateo -e Santa\ Clara -e Solano -e Sonoma | cut -f 1,2,5 -d ,
+cd ../covid-19-data
+git pull &> /dev/null
+cat us-counties.csv | fgrep -e Alameda -e Contra\ Costa -e Marin -e Napa -e San\ Francisco -e San\ Mateo -e Santa\ Clara -e Solano -e Sonoma | cut -f 1,2,5 -d ,
